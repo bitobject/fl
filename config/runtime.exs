3f58acfc273d386b2a1618c80d04c73eb0ee5bb1
Pro <pro@Pros-MacBook-Pro.local>
2022-10-30 02:07:21 +0400

    [SMARTBOT-494] assistant dgraph tree

--- config/runtime.exs
+++ config/runtime.exs
@@ -48,11 +48,8 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
-
   config :fl, FlWeb.Endpoint,
-    url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
+    # url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.

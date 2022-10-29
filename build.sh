6c822d8f81e2027d3373f8582816c5515e1ea46b
Pro <pro@Pros-MacBook-Pro.local>
2022-10-29 23:37:52 +0400

    [SMARTBOT-494] assistant dgraph tree

--- build.sh
+++ build.sh
@@ -6,10 +6,5 @@ set -o errexit
 mix deps.get --only prod
 MIX_ENV=prod mix compile
 
-# Compile assets
-npm install --prefix ./assets
-npm run deploy --prefix ./assets
-mix phx.digest
-
-# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix phx.gen.release
 MIX_ENV=prod mix release --overwrite

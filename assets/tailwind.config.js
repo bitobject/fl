5acc13597686f4a1898e043386451b3af7c11d26
Pro <pro@Pros-MacBook-Pro.local>
2022-11-04 13:18:38 +0400

    [SMARTBOT-473] Изменение свзи сущностей с ассистента на версию

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -10,7 +10,33 @@ module.exports = {
     '../lib/*_web/**/*.*ex'
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        0.98: "0.98vw",
+        0: "0px",
+        1.1: "110%"
+      },
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    fontSize: {
+      body: "0.97vw",
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.25rem" }],
+      base: ["1rem", { lineHeight: "1.5rem" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem" }],
+      xl: ["1.25rem", { lineHeight: "1.75rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["2rem", { lineHeight: "2.375rem" }],
+      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
+      "5xl": ["3rem", { lineHeight: "1" }],
+      "6xl": ["3.75rem", { lineHeight: "1" }],
+      "7xl": ["4.5rem", { lineHeight: "1" }],
+      "8xl": ["6rem", { lineHeight: "1" }],
+      "9xl": ["8rem", { lineHeight: "1" }],
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
